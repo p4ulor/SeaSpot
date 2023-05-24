94d0b99fe3d3433540577ed39338ed18e60a6eb1
RaulJCS5 <rauljosesantos@hotmail.com>
2023-05-24 23:03:54 +0100

    Update web-api.mjs

--- WebApp/web/api/web-api.mjs
+++ WebApp/web/api/web-api.mjs
@@ -3,36 +3,42 @@ import * as codes from '../../utils/errors-and-codes.mjs'
 import { UplinkResponseModel } from './UplinkResponseModel.mjs'
 import { DownlinkQueuedResponseModel } from './DownlinkQueuedResponseModel.mjs'
 
-function api(config, services) {
+export default function (config, services) {
+    
+    // Validate argument
+    if (!services) {
+        throw errors.INVALID_PARAMETER('services')
+    }
+    return {
+        getWebHook: handleRequest(getWebHook),
+        getAllMessages: handleRequest(getAllMessages),
+    }
 
     /**
      * @param {express.Request} req 
      * @param {express.Response} rsp 
      */
     async function getWebHook(req, rsp) {
-        tryCatch(async () => {
-            const { body } = req;
+        const { body } = req;
 
-            // Check if the request contains an uplink message
-            if (body.uplink_message) {
-                const uplinkModel = UplinkResponseModel(body);
-                console.log('Received Uplink:');
-                //console.log(uplinkModel);
-                //console.log(`PAYLOAD = ${base64ToHex(JSON.stringify(uplinkModel.uplink.frmPayload))}`)
-                services.createMessage(uplinkModel.uplink.frmPayload, uplinkModel.deviceId, uplinkModel.applicationId, uplinkModel.correlationIds)
-            }
+        // Check if the request contains an uplink message
+        if (body.uplink_message) {
+            const uplinkModel = UplinkResponseModel(body);
+            console.log('Received Uplink:');
+            //console.log(uplinkModel);
+            //console.log(`PAYLOAD = ${base64ToHex(JSON.stringify(uplinkModel.uplink.frmPayload))}`)
+            services.createMessage(uplinkModel.uplink.frmPayload, uplinkModel.deviceId, uplinkModel.applicationId, uplinkModel.correlationIds)
+        }
+        // Check if the request contains a downlink message
+        if (body.downlink_queued) {
+            const downlinkModel = DownlinkQueuedResponseModel(body);
+            console.log('Received Downlink:');
+            //console.log(downlinkModel);
+            //console.log(`PAYLOAD = ${base64ToHex(JSON.stringify(downlinkModel.downlinkQueued.frmPayload))}`)
+            services.createMessage(downlinkModel.downlinkQueued.frmPayload, downlinkModel.deviceId, downlinkModel.applicationId, downlinkModel.correlationIds)
+        }
 
-            // Check if the request contains a downlink message
-            if (body.downlink_queued) {
-                const downlinkModel = DownlinkQueuedResponseModel(body);
-                console.log('Received Downlink:');
-                //console.log(downlinkModel);
-                //console.log(`PAYLOAD = ${base64ToHex(JSON.stringify(downlinkModel.downlinkQueued.frmPayload))}`)
-                services.createMessage(downlinkModel.downlinkQueued.frmPayload, downlinkModel.deviceId, downlinkModel.applicationId, downlinkModel.correlationIds)
-            }
-
-            rsp.status(codes.statusCodes.OK).json({ ok: "okay" })
-        }, rsp)
+        return { success: true }
     }
 
     /**
@@ -40,10 +46,8 @@ function api(config, services) {
      * @param {express.Response} rsp 
      */
     async function getAllMessages(req, rsp) {
-        tryCatch(async () => {
-            const messages = await services.getAllMessages(); // Await the promise to get the result
-            rsp.status(codes.statusCodes.OK).json(messages)
-        }, rsp)
+        const messages = await services.getAllMessages(); // Await the promise to get the result
+        return messages
     }
 
     /**
@@ -55,23 +59,17 @@ function api(config, services) {
         return buffer.toString('hex')
     }
 
-    return {
-        getWebHook: getWebHook,
-        getAllMessages: getAllMessages
+    function handleRequest(handler) {
+        return async function (req, rsp) {
+            try {
+                let body = await handler(req, rsp)
+                rsp.status(codes.statusCodes.OK).json(body)
+            } catch (e) {
+                if (e.code) rsp.status(e.code).json({ error: e.message })
+                else rsp.status(codes.statusCodes.INTERNAL_SERVER_ERROR).json({ error: e.message })
+            }
+        }
     }
 
 }
 
-async function tryCatch(func, rsp) { //this cuts down 3 lines per api/controller method
-    if (typeof func !== 'function') throw new Error("Can't use this function like this. param 'func' must be a function")
-    try {
-        await func()
-    } catch (e) { //e.code is a property that's added to our list of Exceptions
-        if (e.code) rsp.status(e.code).json({ error: e.message })
-        else rsp.status(codes.statusCodes.INTERNAL_SERVER_ERROR).json({ error: e.message })
-    }
-}
-
-
-
-export default api
